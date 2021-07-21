65391717ae6342c36cb429c55690fbe9deac9769
hasan1970 <66068600+hasan1970@users.noreply.github.com>
2021-07-21 16:09:22 -0400

    Add files via upload

---
+++ Traffic/traffic.py
@@ -0,0 +1,140 @@
+import cv2
+import numpy as np
+import os
+import sys
+import glob
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+
+EPOCHS = 10
+IMG_WIDTH = 30
+IMG_HEIGHT = 30
+NUM_CATEGORIES = 43
+TEST_SIZE = 0.4
+
+
+def main():
+    #load_data(data_dir)
+    
+    
+
+    # Check command-line arguments
+    if len(sys.argv) not in [2, 3]:
+        sys.exit("Usage: python traffic.py data_directory [model.h5]")
+
+    # Get image arrays and labels for all image files
+    images, labels = load_data(sys.argv[1])
+
+    # Split data into training and testing sets
+    labels = tf.keras.utils.to_categorical(labels)
+    x_train, x_test, y_train, y_test = train_test_split(
+        np.array(images), np.array(labels), test_size=TEST_SIZE
+    )
+
+    # Get a compiled neural network
+    model = get_model()
+
+    # Fit model on training data
+    model.fit(x_train, y_train, epochs=EPOCHS)
+
+    # Evaluate neural network performance
+    model.evaluate(x_test,  y_test, verbose=2)
+
+    # Save model to file
+    if len(sys.argv) == 3:
+        filename = sys.argv[2]
+        model.save(filename)
+        print(f"Model saved to {filename}.")
+        
+    
+
+def load_data(data_dir):
+    """
+    Load image data from directory `data_dir`.
+
+    Assume `data_dir` has one directory named after each category, numbered
+    0 through NUM_CATEGORIES - 1. Inside each category directory will be some
+    number of image files.
+
+    Return tuple `(images, labels)`. `images` should be a list of all
+    of the images in the data directory, where each image is formatted as a
+    numpy ndarray with dimensions IMG_WIDTH x IMG_HEIGHT x 3. `labels` should
+    be a list of integer labels, representing the categories for each of the
+    corresponding `images`.
+      """
+    images=[]
+    labels=[]
+    size=(IMG_WIDTH,IMG_HEIGHT)
+    
+    for path, subdirs, files in os.walk(data_dir):    
+        
+        for name in files:
+            label=path[6:]
+            labels.append(label)
+            img=cv2.imread(os.path.join(path,name))
+            img2=cv2.resize(img,size)
+            images.append(img2)
+                       
+    return (images,labels)
+
+def get_model():
+    """
+    Returns a compiled convolutional neural network model. Assume that the
+    `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
+    The output layer should have `NUM_CATEGORIES` units, one for each category.
+    tf.keras.layers.
+
+    """
+    size=(IMG_WIDTH, IMG_HEIGHT, 3)
+    model = tf.keras.models.Sequential([
+
+        #3 convs one pool 3 convs one pool
+        #   MAKE EVERYTHING SIZE then try halfing impwidth
+        #2 32conv pool 62conv pool, d-0.3 GIVES 0,31
+        #1 32CONV POOL 64CONV POOL SAAAAME
+        #16CONV 32CONV 1 POOL 32CONV 1 POOL GIVES 0.36
+
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=size
+         ),
+
+        
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        
+        tf.keras.layers.Conv2D(
+            64, (3, 3), activation="relu", input_shape=size
+         ),
+
+        
+        tf.keras.layers.MaxPooling2D((2, 2)),
+
+
+        # Flatten units
+        tf.keras.layers.Flatten(),
+
+# 3 hidden layers in increasing order of nodes 
+        
+        tf.keras.layers.Dense(100, activation="relu"),
+        tf.keras.layers.Dense(100, activation="relu"),
+        tf.keras.layers.Dense(100, activation="relu"),                 
+
+        tf.keras.layers.Dropout(0.5),
+
+        # Add an output layer with output units
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+
+    return model
+  
+
+if __name__ == "__main__":
+    main()
